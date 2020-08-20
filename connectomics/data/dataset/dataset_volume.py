3bae238010b0f7db649ce76a5795633939964ba8
matinraayai <raayaiardani.m@northeastern.edu>
2020-08-20 13:15:48 -0400

    Minor style fix.

--- connectomics/data/dataset/dataset_volume.py
+++ connectomics/data/dataset/dataset_volume.py
@@ -2,11 +2,10 @@ from __future__ import print_function, division
 import numpy as np
 import random
 
-import torch
 import torch.utils.data
 from ..utils import count_volume, crop_volume, relabel, seg_to_targets, seg_to_weights
 
-# 3d volume dataset class
+
 class VolumeDataset(torch.utils.data.Dataset):
     """
     Dataset class for 3D image volumes.
@@ -34,22 +33,22 @@ class VolumeDataset(torch.utils.data.Dataset):
                  sample_volume_size=(8, 64, 64),
                  sample_label_size=(8, 64, 64),
                  sample_stride=(1, 1, 1),
-                 sample_invalid_thres=[0, 0],
+                 sample_invalid_thres=(0, 0),
                  augmentor=None, 
-                 target_opt=['1'], 
-                 weight_opt=[['1']],
+                 target_opt=('1',),
+                 weight_opt=(('1',),),
                  mode='train',
                  do_2d=False,
                  iter_num=-1,
                  # options for rejection sampling
-                 reject_size_thres= 0,
+                 reject_size_thres=0,
                  reject_after_aug=False, 
-                 reject_p= 0.98):
+                 reject_p=0.98):
 
         self.mode = mode
         self.do_2d = do_2d
         if self.do_2d:
-            assert (sample_volume_size[0]==1) * (sample_label_size[0]==1)
+            assert (sample_volume_size[0] == 1) * (sample_label_size[0] == 1)
         # for partially labeled data
         # m1 (no): sample chunks with over certain percentage
         #   = online version: rejection sampling can be slow
@@ -96,22 +95,22 @@ class VolumeDataset(torch.utils.data.Dataset):
                 for i in range(len(self.sample_num)):
                     seg_bad = np.array([-1]).astype(self.label[i].dtype)[0]
                     if np.any(self.label[i] == seg_bad):
-                        print('dataset %d: needs mask for invalid region'%(i))
+                        print(f'dataset {i}: needs mask for invalid region')
                         self.sample_invalid[i] = True
                         self.sample_num[i] = np.count_nonzero(seg != seg_id)
 
         self.sample_num_a = np.sum(self.sample_num)
         self.sample_num_c = np.cumsum([0] + list(self.sample_num))
 
-        if mode=='test': # for test
+        if mode == 'test':  # for test
             self.sample_size_vol = [np.array([np.prod(x[1:3]), x[2]]) for x in self.sample_size]
        
         # For relatively small volumes, the total number of samples can be generated is smaller
         # than the number of samples required for training (i.e., iteration * batch size). Thus
         # we let the __len__() of the dataset return the larger value among the two during training. 
-        if iter_num < 0: # inference mode
+        if iter_num < 0:  # inference mode
             self.iter_num = self.sample_num_a
-        else: # training mode
+        else:  # training mode
             self.iter_num = max(iter_num, self.sample_num_a)
         print('Total number of samples to be generated: ', self.iter_num)
 
@@ -123,24 +122,24 @@ class VolumeDataset(torch.utils.data.Dataset):
         # sample: need float32
 
         vol_size = self.sample_volume_size
-        if self.mode in ['train','valid']:
+        if self.mode in ['train', 'valid']:
             # train mode
             # if elastic deformation: need different receptive field
             # position in the volume
             pos, out_volume, out_label = self._rejection_sampling(vol_size, 
-                            size_thres=self.reject_size_thres, 
-                            background=0, 
-                            p=self.reject_p)
+                                                                  size_thres=self.reject_size_thres,
+                                                                  background=0,
+                                                                  p=self.reject_p)
 
             # augmentation
             if self.augmentor is not None:  # augmentation
                 if np.array_equal(self.augmentor.sample_size, out_label.shape):
                     # for warping: cv2.remap require input to be float32
                     # make labels index smaller. o/w uint32 and float32 are not the same for some values
-                    data = {'image':out_volume, 'label':out_label}
+                    data = {'image': out_volume, 'label': out_label}
                     augmented = self.augmentor(data)
                     out_volume, out_label = augmented['image'], augmented['label']
-                else: # the data is already augmented in the rejection sampling step
+                else:  # the data is already augmented in the rejection sampling step
                     pass
                 
             if self.do_2d:
@@ -156,10 +155,10 @@ class VolumeDataset(torch.utils.data.Dataset):
         elif self.mode == 'test':
             # test mode
             pos = self._get_pos_test(index)
-            out_volume = (crop_volume(self.volume[pos[0]], vol_size, pos[1:])/255.0).astype(np.float32)
+            out_volume = (crop_volume(self.volume[pos[0]], vol_size, pos[1:]) / 255.0).astype(np.float32)
             if self.do_2d:
                 out_volume = np.squeeze(out_volume) 
-            return pos, np.expand_dims(out_volume,0)
+            return pos, np.expand_dims(out_volume, 0)
 
     #######################################################
     # Position Calculator
@@ -168,7 +167,8 @@ class VolumeDataset(torch.utils.data.Dataset):
     def _index_to_dataset(self, index):
         return np.argmax(index < self.sample_num_c) - 1  # which dataset
 
-    def _index_to_location(self, index, sz):
+    @staticmethod
+    def _index_to_location(index, sz):
         # index -> z,y,x
         # sz: [y*x, x]
         pos = [0, 0, 0]
@@ -197,18 +197,18 @@ class VolumeDataset(torch.utils.data.Dataset):
         # np.random: same seed
         pos = [0, 0, 0, 0]
         # pick a dataset
-        did = self._index_to_dataset(random.randint(0,self.sample_num_a-1))
+        did = self._index_to_dataset(random.randint(0, self.sample_num_a-1))
         pos[0] = did
         # pick a position
         tmp_size = count_volume(self.volume_size[did], vol_size, self.sample_stride)
-        tmp_pos = [random.randint(0,tmp_size[x]-1) * self.sample_stride[x] for x in range(len(tmp_size))]
+        tmp_pos = [random.randint(0, tmp_size[x]-1) * self.sample_stride[x] for x in range(len(tmp_size))]
         if self.sample_invalid[did]:
             # make sure the ratio of valid is high
             seg_bad = np.array([-1]).astype(self.label[did].dtype)[0]
             num_trial = 0
-            while num_trial<self.sample_invalid_thres[1]:
+            while num_trial < self.sample_invalid_thres[1]:
                 out_label = crop_volume(self.label[pos[0]], vol_size, tmp_pos)
-                if (out_label!=seg_bad).sum() >= self.sample_invalid_thres[0]:
+                if (out_label != seg_bad).sum() >= self.sample_invalid_thres[0]:
                     break
                 num_trial += 1
         pos[1:] = tmp_pos 
@@ -226,12 +226,12 @@ class VolumeDataset(torch.utils.data.Dataset):
                     assert np.array_equal(self.augmentor.sample_size, self.sample_label_size)
                     if self.reject_after_aug:
                         # decide whether to reject the sample after data augmentation
-                        data = {'image':out_volume, 'label':out_label}
+                        data = {'image': out_volume, 'label': out_label}
                         augmented = self.augmentor(data)
                         out_volume, out_label = augmented['image'], augmented['label']
 
                         temp = out_label.copy().astype(int)
-                        temp = (temp!=background).astype(int).sum()
+                        temp = (temp != background).astype(int).sum()
                     else:
                         # restrict the foreground mask at the center region after data augmentation
                         z, y, x = self.augmentor.volume_size
@@ -241,9 +241,9 @@ class VolumeDataset(torch.utils.data.Dataset):
 
                         temp = out_label.copy()
                         temp = temp[z_start:z_start+z, y_start:y_start+y, x_start:x_start+x]
-                        temp = (temp!=background).astype(int).sum()
+                        temp = (temp != background).astype(int).sum()
                 else:
-                    temp = (out_label!=background).astype(int).sum()
+                    temp = (out_label != background).astype(int).sum()
 
                 # reject sampling
                 if temp > size_thres:
@@ -251,7 +251,7 @@ class VolumeDataset(torch.utils.data.Dataset):
                 elif random.random() > p:
                     break
 
-            else: # no rejection sampling for the foreground mask
+            else:  # no rejection sampling for the foreground mask
                 break
 
         return pos, out_volume, out_label
