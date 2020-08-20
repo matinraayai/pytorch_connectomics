ce09c0f0dfa1f212a607cd1272a03d9a203a150b
matinraayai <raayaiardani.m@northeastern.edu>
2020-08-20 12:32:52 -0400

    Exposed factory methods from the dataset package.

--- connectomics/data/dataset/__init__.py
+++ connectomics/data/dataset/__init__.py
@@ -9,14 +9,12 @@ from .dataset_volume import VolumeDataset
 from .dataset_tile import TileDataset
 from ..utils import collate_fn_target, collate_fn_test, seg_widen_border, readvol
 
-__all__ = ['VolumeDataset',
-           'TileDataset']
+__all__ = ['VolumeDataset', 'TileDataset', 'create_dataloader', 'create_dataset']
 
 
 def _make_path_list(dir_name, file_name):
     """
-    Concatenate directory path(s) and filenames and return
-    the complete file paths. 
+    Concatenates directory path(s) and filenames and returns the complete file paths.
     """
     assert len(dir_name) == 1 or len(dir_name) == len(file_name)
     if len(dir_name) == 1:
@@ -32,7 +30,7 @@ def _get_input(cfg, mode='train'):
     img_name = _make_path_list(dir_name, img_name)
 
     label = None
-    volume = [None]*len(img_name)
+    volume = [None] * len(img_name)
     if mode == 'train':
         label_name = cfg.DATASET.LABEL_NAME.split('@')
         assert len(label_name) == len(img_name)
@@ -42,14 +40,14 @@ def _get_input(cfg, mode='train'):
     for i in range(len(img_name)):
         volume[i] = readvol(img_name[i])
         print(f"volume shape (original): {volume[i].shape}")
-        if (np.array(cfg.DATASET.DATA_SCALE)!=1).any():
+        if (np.array(cfg.DATASET.DATA_SCALE) != 1).any():
             volume[i] = zoom(volume[i], cfg.DATASET.DATA_SCALE, order=1) 
         volume[i] = np.pad(volume[i], ((cfg.DATASET.PAD_SIZE[0],cfg.DATASET.PAD_SIZE[0]), 
                                        (cfg.DATASET.PAD_SIZE[1],cfg.DATASET.PAD_SIZE[1]), 
                                        (cfg.DATASET.PAD_SIZE[2],cfg.DATASET.PAD_SIZE[2])), 'reflect')
         print(f"volume shape (after scaling and padding): {volume[i].shape}")
 
-        if mode=='train':
+        if mode == 'train':
             label[i] = readvol(label_name[i])
             if (np.array(cfg.DATASET.DATA_SCALE) != 1).any():
                 label[i] = zoom(label[i], cfg.DATASET.DATA_SCALE, order=0) 
@@ -68,23 +66,28 @@ def _get_input(cfg, mode='train'):
     return volume, label
 
 
-def get_dataset(cfg, augmentor, mode='train'):
-    """Prepare dataset for training and inference.
+def create_dataset(cfg, augmentor, mode='train'):
+    """
+    Creates a Pytorch dataset for training and inference using the configuration and augmentor.
+    Args:
+        cfg: YACS Configuration object specifications
+        augmentor:
+        mode: Either 'train' or 'test'
+    Return:
+        a Pytorch Dataset
     """
     assert mode in ['train', 'test']
 
     label_erosion = 0
     sample_label_size = cfg.MODEL.OUTPUT_SIZE
     sample_invalid_thres = cfg.DATASET.DATA_INVALID_THRES
-    augmentor = augmentor
-    topt, wopt = -1, -1
+    t_opt, w_opt = -1, -1
     if mode == 'train':
-        sample_volume_size = cfg.MODEL.INPUT_SIZE
         sample_volume_size = augmentor.sample_size
         sample_label_size = sample_volume_size
         label_erosion = cfg.DATASET.LABEL_EROSION
-        sample_stride = (1,1,1)
-        topt, wopt = cfg.MODEL.TARGET_OPT, cfg.MODEL.WEIGHT_OPT
+        sample_stride = (1, 1, 1)
+        t_opt, w_opt = cfg.MODEL.TARGET_OPT, cfg.MODEL.WEIGHT_OPT
         iter_num = cfg.SOLVER.ITERATION_TOTAL * cfg.SOLVER.SAMPLES_PER_BATCH 
     elif mode == 'test':
         sample_stride = cfg.INFERENCE.STRIDE
@@ -94,24 +97,13 @@ def get_dataset(cfg, augmentor, mode='train'):
     # dataset
     if cfg.DATASET.DO_CHUNK_TITLE == 1:
         label_json = cfg.DATASET.INPUT_PATH + cfg.DATASET.LABEL_NAME if mode == 'train' else ''
-        dataset = TileDataset(chunk_num=cfg.DATASET.DATA_CHUNK_NUM, 
-                              chunk_num_ind=cfg.DATASET.DATA_CHUNK_NUM_IND, 
-                              chunk_iter=cfg.DATASET.DATA_CHUNK_ITER, 
-                              chunk_stride=cfg.DATASET.DATA_CHUNK_STRIDE,
-                              volume_json=cfg.DATASET.INPUT_PATH+cfg.DATASET.IMAGE_NAME, 
-                              label_json=label_json,
-                              sample_volume_size=sample_volume_size, 
-                              sample_label_size=sample_label_size,
-                              sample_stride=sample_stride, 
-                              sample_invalid_thres=sample_invalid_thres,
-                              augmentor=augmentor, 
-                              target_opt=topt, 
-                              weight_opt=wopt, 
-                              mode=mode, 
-                              do_2d=cfg.DATASET.DO_2D,
-                              iter_num=iter_num,
-                              label_erosion=label_erosion, 
-                              pad_size=cfg.DATASET.PAD_SIZE)
+        dataset = TileDataset(chunk_num=cfg.DATASET.DATA_CHUNK_NUM, chunk_num_ind=cfg.DATASET.DATA_CHUNK_NUM_IND,
+                              chunk_iter=cfg.DATASET.DATA_CHUNK_ITER, chunk_stride=cfg.DATASET.DATA_CHUNK_STRIDE,
+                              volume_json=cfg.DATASET.INPUT_PATH + cfg.DATASET.IMAGE_NAME, label_json=label_json,
+                              sample_volume_size=sample_volume_size, sample_label_size=sample_label_size,
+                              sample_stride=sample_stride, sample_invalid_thres=sample_invalid_thres,
+                              augmentor=augmentor, target_opt=t_opt, weight_opt=w_opt, mode=mode, do_2d=cfg.DATASET.DO_2D,
+                              iter_num=iter_num, label_erosion=label_erosion, pad_size=cfg.DATASET.PAD_SIZE)
 
     else:
         if cfg.DATASET.PRE_LOAD_DATA[0] is None:  # load from cfg
@@ -126,8 +118,8 @@ def get_dataset(cfg, augmentor, mode='train'):
                                 sample_stride=sample_stride, 
                                 sample_invalid_thres=sample_invalid_thres, 
                                 augmentor=augmentor, 
-                                target_opt=topt, 
-                                weight_opt=wopt, 
+                                target_opt=t_opt,
+                                weight_opt=w_opt,
                                 mode=mode,
                                 do_2d=cfg.DATASET.DO_2D,
                                 iter_num=iter_num,
@@ -139,10 +131,10 @@ def get_dataset(cfg, augmentor, mode='train'):
     return dataset
 
 
-def build_dataloader(cfg, augmentor, mode='train', dataset=None):
-    """Prepare dataloader for training and inference.
+def create_dataloader(cfg, augmentor, mode='train', dataset=None):
     """
-    print('Mode: ', mode)
+    Prepare dataloader for training and inference.
+    """
     assert mode in ['train', 'test']
 
     shuffle = mode == 'train'
@@ -155,10 +147,10 @@ def build_dataloader(cfg, augmentor, mode='train', dataset=None):
         batch_size = cfg.INFERENCE.SAMPLES_PER_BATCH
 
     if dataset is None:
-        dataset = get_dataset(cfg, augmentor, mode)
+        dataset = create_dataset(cfg, augmentor, mode)
     
-    img_loader = torch.utils.data.DataLoader(
+    data_loader = torch.utils.data.DataLoader(
           dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=cf,
           num_workers=cfg.SYSTEM.NUM_CPUS, pin_memory=True)
 
-    return img_loader
+    return data_loader
